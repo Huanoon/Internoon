ac168c1ceeb537ce437b15639c0a71e9d3b5f600
Huanoon <117714936+Huanoon@users.noreply.github.com>
2023-03-04 22:41:48 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	/*double foot;
+	double inch;
+	printf_s("请输入身高：\n");
+	scanf_s("%lf %lf", &foot, &inch);
+	printf_s("身高是%f米\n", (foot + inch / 12) * 0.3048);*/
+
+	//printf("请输入两个整数：");
+	////scanf_s("%d %d", &a, &b);
+	//printf("%d + %d = %d\n", a, b, a + b);
+
+	return 0;
+}
